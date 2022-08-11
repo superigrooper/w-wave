bdeeb71f0f0a5a189a43da195ed9090bfab7819f
superigrooper <superigrooper@gmail.com>
2022-08-11 16:32:43 +0300

    commit:

--- src/js/programs.js
+++ src/js/programs.js
@@ -1,3 +1,10 @@
+/**
+ *
+ * Сравнивает data-атрибут у выбранного селекта и
+ * если он совпадает с data-атрибутом блока с программами,
+ * то данный блок отображается
+ *
+ * */
 export function programs() {
   const select = document.querySelector('.choices');
   const contentItems = document.querySelectorAll('.programs__block');
