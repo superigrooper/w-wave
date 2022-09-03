fd4370dbe635bb133aad129495d16ae2b489fdc8
vitalii_terentev_suchkov <tevvit@gmail.com>
2022-09-03 14:35:23 +0300

    Готовы состояния и пÐразметка карточек в передачах

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,6 @@ import {programs} from "./programs.js";
 import {toggleClassOnClick} from "./toggleClassOnClick.js";
 
 /*========== Подкасты ==========*/
-  const podcastBtns = document.querySelectorAll('.card-podcast__play');
+const podcastBtns = document.querySelectorAll('.card-podcast__play');
 toggleClassOnClick(podcastBtns, 'card-podcast__play--active');
 programs();
