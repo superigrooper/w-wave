25c5c17e680637a9d06b23695f875d9e323b2c39
vitalii_terentev_suchkov <tevvit@gmail.com>
2022-09-05 18:29:14 +0300

    Готов аккордлиееон

--- src/js/main.js
+++ src/js/main.js
@@ -5,3 +5,4 @@ import {toggleClassOnClick} from "./toggleClassOnClick.js";
 const podcastBtns = document.querySelectorAll('.card-podcast__play');
 toggleClassOnClick(podcastBtns, 'card-podcast__play--active');
 programs();
+new Accordion('.accordion__list');
