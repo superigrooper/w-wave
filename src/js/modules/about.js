4940bf0b475ac06c76b7a9fac26697fe15dc54fb
superigrooper <superigrooper@gmail.com>
2022-12-27 17:47:18 +0300

    edit: about section done

---
+++ src/js/modules/about.js
@@ -0,0 +1,8 @@
+const aboutSwiper = new Swiper('.about__swiper', {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+})
